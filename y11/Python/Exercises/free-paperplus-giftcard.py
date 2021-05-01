90e920833c3a94fa4596b64b2779204ecf68f56b
TheTrueCoder <38576090+TheTrueCoder@users.noreply.github.com>
2021-05-01 13:59:00 +1200

    Update free-paperplus-giftcard.py

---
+++ y11/Python/Exercises/free-paperplus-giftcard.py
@@ -0,0 +1,5 @@
+start_card_balance = 50 #Amount of money on gift card initially in dollars.
+book_price = 35 #Dollars
+end_card_balance = start_card_balance - book_price
+print(f"The giftcard's original balance is ${float(start_card_balance)}\nThe book's price is ${float(book_price)}\nSo, The final card balance is ${float(end_card_balance)}")
+#The float conversions make it match the example, although they are redundant.
